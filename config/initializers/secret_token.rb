b51e2f82e14ca63fff13d6d52d571010784902a0
Jason Lee <jlsync@gmail.com>
2012-08-04 11:59:50 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioPusherPong::Application.config.secret_token = '23895c39e488701102846f97289c46007d1a1d670c5d7a2bc1e9d5301fd7d003954f1eacd6ee6464e851a6e34da3a0754a5a11e1c3b45b0be61889111bba5e04'
